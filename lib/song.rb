f0cdacc3b368c984a61c8ffca3d8ad267751668f
Derek phung <dragevards@gmail.com>
2020-05-03 21:59:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,7 +16,7 @@ class Song
   def add_song
   
   def self.new_by_filename(file)
-     song = file_name.split(" - ")[1]
+    song = file_name.split(" - ")[1]
     artist = file_name.split(" - ")[0]
     new_song = self.new(song)
     new_song.artist_name = artist
